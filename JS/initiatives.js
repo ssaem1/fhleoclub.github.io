5c349d8d95219d9dee749c1faa1bfb6cc1ff29d9
Sam Shan <samwam360@gmail.com>
2023-05-18 19:26:08 -0700

    huge update B)

--- JS/initiatives.js
+++ JS/initiatives.js
@@ -4,3 +4,14 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+document.querySelectorAll('.gallery-element').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-image span').onclick = () =>{
+    document.querySelector('.popup-image').style.display = 'none';
+}
